4e6df4d789b9b601629a345d2e23ba1a8e24e625
Waribu George <gwaribu@gmail.com>
2022-07-06 23:22:23 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  console.log("Request Date:" + new Date());
+  next();
+});
+
+app.use(function (req, res, next) {
+  var filepath = path.join(__dirname, "static", req.url);
+  fs.stat(filepath, function (err, fileInfo) {
+    if (err) {
+      next();
+      return;
+    }
+    if (fileInfo.isFile()) {
+      res.sendFile(filepath);
+    } else {
+      next();
+    }
+  });
+});
+
+app.use(function (req, res) {
+  res.status(404);
+  res.send("File Not Found Buddy!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
